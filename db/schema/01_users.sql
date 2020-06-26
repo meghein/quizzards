4f67c45fc1ac110ddaf4fa3a212666d4eca3bc43
Meghan Hein <meghan.hein@gmail.com>
2020-06-26 21:36:05 +0000

    create db schemas

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,8 +1,11 @@
 -- Drop and recreate Users table (Example)
+DROP TABLE IF EXISTS users
+CASCADE;
 
-DROP TABLE IF EXISTS users CASCADE;
-CREATE TABLE users (
+CREATE TABLE users
+(
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
-  password VARCHAR(255)
+  email VARCHARR(100) NOT NULL,
+  password VARCHAR(255) NOT NULL
 );
