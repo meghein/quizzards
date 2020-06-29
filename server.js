b0f16b6fbd511cf5135bd4f919a91972b2346801
Graham Mothersill <graham.mothersill@gmail.com>
2020-06-29 21:09:49 +0000

    add new_quiz_container appearence toggle

--- server.js
+++ server.js
@@ -46,11 +46,8 @@ const loginRoute = require("./routes/login_route");
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/users", usersRoutes(dbHelpers));
-<<<<<<< HEAD
-=======
 // app.use("/api/widgets", widgetsRoutes(dbHelpers));
 // app.use("/quizzes", quizRoutes(dbHelpers));
->>>>>>> feature_new_quiz
 app.use("/login", loginRoute(dbHelpers));
 app.use("/quizzes", quizRoutes(dbHelpers));
 
