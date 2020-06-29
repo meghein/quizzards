d86848d8d50be73cad549c4f42b54ef06866d177
Meghan Hein <meghan.hein@gmail.com>
2020-06-29 22:35:38 +0000

    rework response route

--- routes/response_route.js
+++ routes/response_route.js
@@ -5,8 +5,12 @@ module.exports = ({addUserResults}
 ) => {
 
   router.post("/", (req, res) => {
-
-    console.log(req.body);
+    const templateVars = {
+      user: req.session["user"],
+      userId: req.session["user_id"] ? req.session["user_id"] : undefined,
+      quizResponse: req.body
+    };
+    console.log(templateVars);
 
     res.redirect("/");
   });
