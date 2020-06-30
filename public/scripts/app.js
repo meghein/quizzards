b1319b5f4d461a14a4881f10c31746ce077b8976
bobby-brice <bobby.brice@gmail.com>
2020-06-30 22:38:44 +0000

    quiz-id style and update nav/header

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -82,10 +82,7 @@ $(document).ready(() => {
   // SPECIFIC QUIZ RENDER //
 
 
-  $("#fade").modal({
-    fadeDuration: 1000,
-    fadeDelay: 1.75 // Will fade in 750ms after the overlay finishes.
-  });
+
 
 
   $("#get_all_quizzes").on('click', function() {
